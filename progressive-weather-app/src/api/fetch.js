2015f4a1de1bf7c0d9fb3324ab03d5ecbcbf4070
rorieArmstrong <61753930+rorieArmstrong@users.noreply.github.com>
2020-10-14 09:58:04 +0100

    add api key

--- progressive-weather-app/src/api/fetch.js
+++ progressive-weather-app/src/api/fetch.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
-const API_KEY = 'f33a484cf794d08d0148764789aaba32';
+const API_KEY = '9da553dc1200b6185ea97093f760eaad';
 
 const fetch = async (querry) => {
     const {res} = await axios.get(URL, {
